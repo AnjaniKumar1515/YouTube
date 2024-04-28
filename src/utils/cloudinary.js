e6e3bd5e541c6b2e9d2837f28c1906f95ea40b49
AnjaniRai <shadowarthur1019@gmail.com>
2024-04-28 20:29:02 +0530

    Add multer middleware and cloudinary utility functions

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadToCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //Upload file to cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //File has been uploaded successfully
+    console.log(
+      "File has been successfully uploaded to cloudinary ⛈️",
+      response.url
+    );
+    return response;
+  } catch {
+    fs.unlinkSync(localFilePath); //Remove the locally saved temporary file if it fails to upload to cloudinary
+    return null
+  }
+};
+
+cloudinary.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result);
+  }
+);
+export {uploadToCloudinary}
